a315b5c82eb9455518b037af63d7048fb942791e
DhruvBajaj01 <71027506+DhruvBajaj01@users.noreply.github.com>
2022-01-24 13:10:16 +0530

    Add files via upload

---
+++ pattern/pn.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    bool flag=0;
+    for(int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<"not prime"<<endl;
+            flag=1;
+            break;
+        }
+        
+
+    }
+    if(flag==0){
+        cout<<"prime";
+    }
+    return 0;
+}
